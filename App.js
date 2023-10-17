7b7fa0b1c597e77a39d0f8970f7610cf8a18a0ae
Sebastian Basel <sebastian.basel@dci.education>
2023-10-17 17:02:30 +0200

    Update expo

--- App.js
+++ App.js
@@ -1,29 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootNavigator } from './RootNavigator'; 
-import  { useState } from 'react';
-import { AppLoading } from 'expo';
-import * as Font from 'expo-font';
-
-async function loadFonts() {
-  await Font.loadAsync({
-    'Great Vibes': require('./assets/fonts/GreatVibes-Regular.ttf'),
-  });
-}
 
 export default function Main() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  if (!fontsLoaded) {
-    return (
-      <AppLoading
-        startAsync={loadFonts}
-        onFinish={() => setFontsLoaded(true)}
-        onError={console.warn}
-      />
-    );
-  }
-
   return (
     <NavigationContainer>
       <RootNavigator />
