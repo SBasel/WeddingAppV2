bb6d217c4db4c1998e639c6945e9fc3c489e1d68
Sebastian Basel <sebastian.basel@dci.education>
2023-10-18 13:23:45 +0200

    Text2

--- comp/Camera/ImageEditor.js
+++ comp/Camera/ImageEditor.js
@@ -1,6 +1,6 @@
-import React, { useState, useRef } from 'react';
-import { View, Image, TextInput, StyleSheet, TouchableOpacity, Text, Dimensions, PanResponder, KeyboardAvoidingView, Platform } from 'react-native';
-import { FontAwesome5 } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { View, Image, TextInput, StyleSheet, TouchableOpacity, Text, Dimensions, PanResponder } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons'; 
 
 export function ImageEditor({ route, navigation }) {
     const { imageUri } = route.params;
@@ -8,21 +8,22 @@ export function ImageEditor({ route, navigation }) {
     const [text, setText] = useState('');
     const [fontSize, setFontSize] = useState(20);
     const [textPosition, setTextPosition] = useState({ x: 50, y: 50 });
+    const [offsetX, setOffsetX] = useState(0);
+    const [offsetY, setOffsetY] = useState(0);
 
-    const panResponder = useRef(
-        PanResponder.create({
-            onStartShouldSetPanResponder: () => true,
-            onPanResponderMove: (event, gestureState) => {
-                setTextPosition({
-                    x: textPosition.x + gestureState.dx,
-                    y: textPosition.y + gestureState.dy
-                });
-            },
-            onPanResponderRelease: () => {
-                // Optional: Hier können Sie zusätzlichen Code hinzufügen, um die Endposition zu speichern.
-            }
-        })
-    ).current;
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: (evt, gestureState) => {
+            setOffsetX(textPosition.x - gestureState.x0);
+            setOffsetY(textPosition.y - gestureState.y0);
+        },
+        onPanResponderMove: (evt, gestureState) => {
+            setTextPosition({
+                x: gestureState.moveX + offsetX,
+                y: gestureState.moveY + offsetY
+            });
+        },
+    });
 
     const onClose = () => {
         navigation.goBack();
@@ -33,48 +34,40 @@ export function ImageEditor({ route, navigation }) {
     }
 
     return (
-        <KeyboardAvoidingView
-            style={{ flex: 1 }}
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-        >
-            <View style={styles.editorContainer}>
-                <Image source={{ uri: imageUri }} style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height - 100 }} />
-                
-                {isEditing && (
-                    <TextInput
-                        style={{...styles.textInput, fontSize: fontSize, top: textPosition.y, left: textPosition.x}}
-                        value={text}
-                        onChangeText={setText}
-                        placeholder="Füge Text hinzu"
-                    />
+        <View style={styles.editorContainer}>
+            <Image source={{ uri: imageUri }} style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height - 100 }} />
+            
+            {isEditing && (
+                <TextInput
+                    {...panResponder.panHandlers}
+                    style={{...styles.textInput, fontSize: fontSize, left: textPosition.x, top: textPosition.y}}
+                    value={text}
+                    onChangeText={setText}
+                    placeholder="Füge Text hinzu"
+                    multiline={true}
+                />
+            )}
+            
+            {!isEditing && (
+                <Text {...panResponder.panHandlers} style={{...styles.overlayText, fontSize: fontSize, left: textPosition.x, top: textPosition.y}}>{text}</Text>
+            )}
+            
+            <View style={styles.buttonContainer}>
+                {isEditing ? (
+                    <TouchableOpacity onPress={onSave}>
+                        <FontAwesome5 name="check" size={24} color="green" />
+                    </TouchableOpacity>
+                ) : (
+                    <TouchableOpacity onPress={() => setIsEditing(true)}>
+                        <FontAwesome5 name="pen" size={24} color="blue" />
+                    </TouchableOpacity>
                 )}
-                
-                {!isEditing && (
-                    <Text
-                        style={{...styles.overlayText, fontSize: fontSize, top: textPosition.y, left: textPosition.x}}
-                        {...panResponder.panHandlers}
-                    >
-                        {text}
-                    </Text>
-                )}
-                
-                <View style={styles.buttonContainer}>
-                    {isEditing ? (
-                        <TouchableOpacity onPress={onSave}>
-                            <FontAwesome5 name="check" size={24} color="green" />
-                        </TouchableOpacity>
-                    ) : (
-                        <TouchableOpacity onPress={() => setIsEditing(true)}>
-                            <FontAwesome5 name="pen" size={24} color="blue" />
-                        </TouchableOpacity>
-                    )}
-                </View>
-
-                <TouchableOpacity style={styles.closeButton} onPress={onClose}>
-                    <FontAwesome5 name="times" size={24} color="black" />
-                </TouchableOpacity>
             </View>
-        </KeyboardAvoidingView>
+
+            <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+                <FontAwesome5 name="times" size={24} color="black" />
+            </TouchableOpacity>
+        </View>
     );
 }
 
@@ -85,13 +78,16 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     textInput: {
-        position: 'absolute',
-        color: 'black',
-        borderColor: 'black',
-        borderWidth: 1,
-        borderRadius: 4,
-        zIndex: 5, 
-    },
+    position: 'absolute',
+    color: 'black',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 4,
+    zIndex: 5,
+    paddingHorizontal: 5, 
+    minWidth: 150, 
+    maxWidth: Dimensions.get('window').width - 40,
+},
     overlayText: {
         position: 'absolute',
         color: 'black',
