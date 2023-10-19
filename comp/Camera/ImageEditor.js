e522577a1d06329d6309eee482eb95b398b29015
Sebastian Basel <sebastian.basel@dci.education>
2023-10-19 11:07:33 +0200

    V2

--- comp/Camera/ImageEditor.js
+++ comp/Camera/ImageEditor.js
@@ -1,11 +1,12 @@
 import React, { useState, useRef } from 'react';
-import { View, Image, TextInput, StyleSheet, TouchableOpacity, Text, Dimensions, PanResponder, KeyboardAvoidingView, Platform, Modal, ActivityIndicator, ScrollView } from 'react-native';
+import { View, Image, TextInput, StyleSheet, TouchableOpacity, Text, Dimensions, PanResponder, KeyboardAvoidingView, Platform, Modal, ActivityIndicator, Slider, Picker } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { uploadImage } from './UploadImage.js';
 import ViewShot from "react-native-view-shot";
 import { FontColorDropdown } from './fontHandler/FontColorDropdown.js';
 import { FontFamilyDropdown } from './fontHandler/FontFamilyDropdown.js';
 import { FontSizeDropdown } from './fontHandler/FontSizeDropdown.js';
+
 
 export function ImageEditor({ route, navigation }) {
     const { imageUri } = route.params;
@@ -21,25 +22,27 @@ export function ImageEditor({ route, navigation }) {
     const [isModalVisible, setModalVisible] = useState(false);
     const [isTextModalVisible, setTextModalVisible] = useState(false);
     const [textColor, setTextColor] = useState('black'); 
-    const [fontFamily, setFontFamily] = useState('Arial');
-    const [scrollEnabled, setScrollEnabled] = useState(true);
+    const [fontFamily, setFontFamily] = useState('Arial'); 
+
+
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
-        onMoveShouldSetPanResponder: () => true,
-        onStartShouldSetPanResponderCapture: () => true,
-        onMoveShouldSetPanResponderCapture: () => true,
+        onMoveShouldSetPanResponder: () => true,  // Hinzu
+        onStartShouldSetPanResponderCapture: () => true,  // Hinzu
+        onMoveShouldSetPanResponderCapture: () => true,  // Hinzu
         onPanResponderGrant: (evt, gestureState) => {
-        setOffsetX(textPosition.x - gestureState.x0);
-        setOffsetY(textPosition.y - gestureState.y0);
+            setOffsetX(textPosition.x - gestureState.x0);
+            setOffsetY(textPosition.y - gestureState.y0);
         },
         onPanResponderMove: (evt, gestureState) => {
-        setTextPosition({
-            x: gestureState.moveX + offsetX,
-            y: gestureState.moveY + offsetY
-        });
-    },
+            setTextPosition({
+                x: gestureState.moveX + offsetX,
+                y: gestureState.moveY + offsetY
+            });
+        },
     });
+
 
     const onClose = () => {
         navigation.goBack();
@@ -68,161 +71,84 @@ export function ImageEditor({ route, navigation }) {
         setIsLoading(false);
     }
 
+
+
     const captureImageWithText = async () => {
         try {
             const dataUri = await viewShotRef.current.capture({ format: 'base64' });
             console.log("Bild erfolgreich erfasst!");
+            console.log("Test", dataUri)
             onDiskSave(dataUri);
         } catch (error) {
             console.log("Fehler beim Erfassen des Bildes: ", error);
         }
     };
 
-    // Render return
+
+
+
+
+
     return (
-        <ScrollView style={{ flex: 1 }} bounces={false} scrollEnabled={scrollEnabled}>
         <KeyboardAvoidingView 
-            style={{ flex: 1 }} 
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }} 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
         >
-            <View style={styles.editorContainer}>
-                <ViewShot ref={viewShotRef} options={{ format: "png", quality: 1.0, result: "data-uri" }}>
-                    <Image source={{ uri: imageUri }} style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height - 100 }} />
-                    {isEditing && (
-                        <TextInput
-                            style={{...styles.textInput, fontSize: fontSize, left: textPosition.x, top: textPosition.y}}
-                            value={text}
-                            onChangeText={setText}
-                            placeholder="Füge Text hinzu"
-                            multiline={true}
-                        />
-                    )}
-                    {!isEditing && (
-                        <Text {...panResponder.panHandlers} style={{...styles.overlayText, fontSize: fontSize, color: textColor, fontFamily: fontFamily, left: textPosition.x, top: textPosition.y}}>{text}</Text>
-                    )}
-                </ViewShot>
-                <View style={styles.buttonContainer}>
-                    {isEditing ? (
-                        <TouchableOpacity onPress={onSave}>
-                            <FontAwesome5 name="check" size={24} color="green" />
+        <View style={styles.editorContainer}>
+            <ViewShot ref={viewShotRef} options={{ format: "png", quality: 1.0, result: "data-uri" }}>
+            <Image source={{ uri: imageUri }} style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height - 100 }} />
+            
+            {isEditing && (
+                <TextInput
+                    style={{...styles.textInput, fontSize: fontSize, left: textPosition.x, top: textPosition.y}}
+                    value={text}
+                    onChangeText={setText}
+                    placeholder="Füge Text hinzu"
+                    multiline={true}
+                />
+            )}
+            
+            {!isEditing && (
+                <Text {...panResponder.panHandlers} style={{...styles.overlayText, fontSize: fontSize, color: textColor, fontFamily: fontFamily, left: textPosition.x, top: textPosition.y}}>{text}</Text>
+            )}
+            </ViewShot>
+            <View style={styles.buttonContainer}>
+                {isEditing ? (
+                    <TouchableOpacity onPress={onSave}>
+                        <FontAwesome5 name="check" size={24} color="green" />
+                    </TouchableOpacity>
+                ) : (
+                    <>
+                    <View style={styles.editcon}>
+                        <TouchableOpacity onPress={() => setTextModalVisible(true)} style={styles.editb} >
+                            <FontAwesome5 name="pen" size={24} />
                         </TouchableOpacity>
-                    ) : (
-                        <>
-                            <View style={styles.editcon}>
-                                <TouchableOpacity onPress={() => setTextModalVisible(true)} style={styles.editb} >
-                                    <FontAwesome5 name="pen" size={24} />
-                                </TouchableOpacity>
-                            </View>  
-                            <View style={styles.editcon}>    
-                                <TouchableOpacity onPress={captureImageWithText } style={styles.editb}>
-                                    <FontAwesome5 name="save" size={24}   />
-                                </TouchableOpacity>
-                            </View>
-                        </>
-                    )}
-                </View>
-                <TouchableOpacity style={styles.closeButton} onPress={onClose}>
-                    <FontAwesome5 name="times" size={24} color="black" />
-                </TouchableOpacity>
-                {capturedImage && (
-                    <Image source={{ uri: capturedImage }} style={{ width: 100, height: 100, position: 'absolute', bottom: 10, right: 10, zIndex: -999}} />
+                        </View>  
+                    <View style={styles.editcon}>    
+                        <TouchableOpacity onPress={captureImageWithText } style={styles.editb}>
+                            <FontAwesome5 name="save" size={24}   />
+                        </TouchableOpacity>
+                        </View>
+                    </>
                 )}
-                {isLoading && (
-                    <View style={{...styles.centered, position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'rgba(0,0,0,0.5)'}}>
-                        <ActivityIndicator size="large" color="#0000ff" />
-                    </View>
-                )}
-                <Modal
-            animationType="slide"
-            transparent={true}
-            visible={isTextModalVisible}
-            onRequestClose={() => {
-                setTextModalVisible(!isTextModalVisible);
-            }}
-        >
-            <View style={styles.inputText}>
-                <View style={styles.modalInputText}>
-                    {/* Dropdown-Container */}
-                    <View style={styles.dropdownContainer}>
-                        {/* Schriftgröße ändern */}
-                        <FontSizeDropdown style={styles.dropdownItem}
-                            value={fontSize}
-                            onFontSizeChange={value => setFontSize(value)}
-                        />
-
-                        {/* Textfarbe ändern */}
-                        <FontColorDropdown style={styles.dropdownItem}
-                            color={textColor}
-                            onFontColorChange={color => setTextColor(color)}
-                        />
-
-                        {/* Schriftart ändern */}
-                        <FontFamilyDropdown style={styles.dropdownItem}
-                            fontFamily={fontFamily}
-                            onFontFamilyChange={font => setFontFamily(font)}
-                        />
-                    </View>
-
-                    {/* TextInput */}
-                    <TextInput
-                        value={text}
-                        onChangeText={setText}
-                        placeholder="Füge Text hinzu"
-                        multiline={true}
-                        style={{ 
-                            color: textColor, 
-                            fontSize: fontSize, 
-                            fontFamily: fontFamily, 
-                            width: '100%', // Damit es die volle Breite ausfüllt
-                            borderColor: '#ccc',
-                            borderWidth: 1,
-                            marginBottom: 20, // Abstand zwischen Input-Feld und Button
-                        }}
-                    />
-                    
-                    {/* Bestätigen Button */}
-                    <TouchableOpacity
-                        style={{ marginTop: 10 }}
-                        onPress={() => {
-                            setTextModalVisible(!isTextModalVisible);
-                        }}
-                    >
-                        <FontAwesome5 name="check" size={24} color="black" />
-                    </TouchableOpacity>
-                </View>
             </View>
-        </Modal>
 
-
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={isModalVisible}
-            onRequestClose={() => {
-                setModalVisible(!isModalVisible);
-            }}
-        >
-            <View style={styles.centered}>
-                <View style={styles.modalView}>
-                    <Text>Bild erfolgreich gespeichert!</Text>
-                    <TouchableOpacity
-                        style={{ marginTop: 10 }}
-                        onPress={() => {
-                            setModalVisible(!isModalVisible);
-                            navigation.navigate('Camera'); // Navigiere zurück zur Camera
-                        }}
-                    >
-                        <Text>OK</Text>
-                    </TouchableOpacity>
-                </View>
+            <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+                <FontAwesome5 name="times" size={24} color="black" />
+            </TouchableOpacity>
+            {capturedImage && (
+            <Image source={{ uri: capturedImage }} style={{ width: 100, height: 100, position: 'absolute', bottom: 10, right: 10, zIndex: -999}} />
+        )}
+        {isLoading && (
+            <View style={{...styles.centered, position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, backgroundColor: 'rgba(0,0,0,0.5)'}}>
+                <ActivityIndicator size="large" color="#0000ff" />
             </View>
-        </Modal>
-            </View>
+        )}
+        
+        </View>
         </KeyboardAvoidingView>
-        </ScrollView>
     );
 }
-
 
 const styles = StyleSheet.create({
     editorContainer: {
@@ -235,14 +161,14 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     borderRadius: 4,
-    zIndex: 9999,
+    zIndex: 5,
     paddingHorizontal: 5, 
     minWidth: 150, 
     maxWidth: Dimensions.get('window').width - 40,
 },
     overlayText: {
         position: 'absolute',
-        zIndex: 9999, 
+        zIndex: 5, 
     },
     buttonContainer: {
         position: 'absolute',
