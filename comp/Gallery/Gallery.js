f825b70908bbbc6291527369f639c07d485d8bbd
Sebastian Basel <sebastian.basel@dci.education>
2023-10-21 13:20:58 +0200

    VideoGallery

--- comp/Gallery/Gallery.js
+++ comp/Gallery/Gallery.js
@@ -1,17 +1,19 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Image, StyleSheet, FlatList, Modal, TouchableOpacity } from 'react-native';
+import { View, Image, StyleSheet, FlatList, Modal, TouchableOpacity} from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
+import ReactPlayer from 'react-player';
 
 export function Gallery() {
-    const [images, setImages] = useState([]);
-    const [selectedImage, setSelectedImage] = useState(null);
-    const [showModal, setShowModal] = useState(false);
+    const [combinedData, setCombinedData] = useState([]);
+    const [selectedMedia, setSelectedMedia] = useState(null);
+    const [showImageModal, setShowImageModal] = useState(false);
+    const [showVideoModal, setShowVideoModal] = useState(false);
     const [isPlaying, setIsPlaying] = useState(false);
     const slideShowInterval = useRef(null);
 
     useEffect(() => {
-        fetchImages();
-    });
+        fetchMedia();
+    }, []);
 
     useEffect(() => {
         if (isPlaying) {
@@ -23,75 +25,95 @@ export function Gallery() {
                 clearInterval(slideShowInterval.current);
             };
         }
-    }, [isPlaying, selectedImage]);
+    }, [isPlaying, selectedMedia]);
 
-    const fetchImages = async () => {
+    const fetchMedia = async () => {
         try {
-            const response = await fetch('https://www.sbdci.de/kpw/get_images.php'); 
-            const data = await response.json();
-            setImages(data);
+            const imgResponse = await fetch('https://www.sbdci.de/kpw/get_images.php');
+            const vidResponse = await fetch('https://www.sbdci.de/kpw/get_vid.php');
+            const imgs = await imgResponse.json();
+            const vids = await vidResponse.json();
+
+            setCombinedData([...imgs, ...vids]);
         } catch (error) {
-            console.error("Error fetching images:", error);
+            console.error("Error fetching media:", error);
         }
     };
 
-    const handleImagePress = (image) => {
-        setSelectedImage(image);
-        setShowModal(true);
+    const handleMediaPress = (media) => {
+        setSelectedMedia(media);
+        if (media.endsWith('.mp4')) {
+            setShowVideoModal(true);
+        } else {
+            setShowImageModal(true);
+        }
     }
-
-    const handleNext = () => {
-        const currentIndex = images.indexOf(selectedImage);
-        const nextIndex = (currentIndex + 1) % images.length;
-        setSelectedImage(images[nextIndex]);
-    }
-
-    const handlePrev = () => {
-        const currentIndex = images.indexOf(selectedImage);
-        const prevIndex = (currentIndex - 1 + images.length) % images.length;
-        setSelectedImage(images[prevIndex]);
-    }
-
     const togglePlay = () => {
         setIsPlaying(!isPlaying);
     }
 
+    const handleNext = () => {
+        const currentIndex = combinedData.indexOf(selectedMedia);
+        const nextIndex = (currentIndex + 1) % combinedData.length;
+        setSelectedMedia(combinedData[nextIndex]);
+    }
+
+    const handlePrev = () => {
+        const currentIndex = combinedData.indexOf(selectedMedia);
+        const prevIndex = (currentIndex - 1 + combinedData.length) % combinedData.length;
+        setSelectedMedia(combinedData[prevIndex]);
+    }
+
     const closeModal = () => {
-        setShowModal(false);
+        setShowImageModal(false);
+        setShowVideoModal(false);
         setIsPlaying(false);
         clearInterval(slideShowInterval.current);
     }
 
+    const renderMediaItem = ({ item }) => {
+    if (item.endsWith('.mp4')) {
+        return (
+            <TouchableOpacity onPress={() => handleMediaPress(item)}>
+                <View style={{ ...styles.image, justifyContent: 'center', alignItems: 'center', backgroundColor: 'grey' }}>
+                    <FontAwesome name="play" size={48} color="white" style={{ position: 'absolute' }} />
+                </View>
+            </TouchableOpacity>
+        );
+    } else {
+        return (
+            <TouchableOpacity onPress={() => handleMediaPress(item)}>
+                <Image 
+                    source={{ uri: `https://www.sbdci.de/kpw/upload/${item}` }} 
+                    style={styles.image} 
+                />
+            </TouchableOpacity>
+        );
+    }
+};
+
+
     return (
         <View style={styles.container}>
             <FlatList
-                data={images}
+                data={combinedData}
                 keyExtractor={(item) => item}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => handleImagePress(item)}>
-                        <Image 
-                            source={{ uri: `https://www.sbdci.de/kpw/upload/${item}` }} 
-                            style={styles.image} 
-                        />
-                    </TouchableOpacity>
-                )}
+                renderItem={renderMediaItem}
                 numColumns={3} 
             />
-            
-            {showModal && (
+
+            {showImageModal && (
                 <Modal
                     animationType="slide"
                     transparent={false}
-                    visible={showModal}
+                    visible={showImageModal}
                 >
                     <View style={styles.modalContainer}>
                         <FontAwesome name="times" size={24} color="black" style={styles.closeIcon} onPress={closeModal} />
-
                         <Image 
-                            source={{ uri: `https://www.sbdci.de/kpw/upload/${selectedImage}` }} 
+                            source={{ uri: `https://www.sbdci.de/kpw/upload/${selectedMedia}` }} 
                             style={styles.modalImage} 
                         />
-
                         <View style={styles.controls}>
                             <FontAwesome name="arrow-left" size={24} color="black" onPress={handlePrev} />
                             <FontAwesome name={isPlaying ? "pause" : "play"} size={24} color="black" onPress={togglePlay} />
@@ -100,6 +122,25 @@ export function Gallery() {
                     </View>
                 </Modal>
             )}
+
+            {showVideoModal && (
+    <Modal
+        animationType="slide"
+        transparent={false}
+        visible={showVideoModal}
+    >
+        <View style={styles.modalContainer}>
+            <FontAwesome name="times" size={24} color="black" style={styles.closeIcon} onPress={closeModal} />
+            <ReactPlayer 
+                url={`https://www.sbdci.de/kpw/upload/${selectedMedia}`} 
+                style={styles.modalVideoContainer}
+                controls={true}
+                width="90%"
+                height="70%"
+            />
+        </View>
+    </Modal>
+)}
         </View>
     );
 }
@@ -127,6 +168,10 @@ const styles = StyleSheet.create({
         height: '70%',
         resizeMode: 'contain',
     },
+    modalVideoContainer: {
+        width: '90%',
+        height: '70%',
+    },
     controls: {
         flexDirection: 'row',
         justifyContent: 'space-between',
@@ -139,6 +184,9 @@ const styles = StyleSheet.create({
         right: 20,
     },
 });
+
+
+
 
 
 
